ceceb334c023cb474dd7bc8d66aa21b020c1cf9d
QUADRI BASIT AYOMIDE <115375709+ScriptPythonic@users.noreply.github.com>
2024-10-10 18:03:27 +0300

    Update vite.config.js

--- Promoat/vite.config.js
+++ Promoat/vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Externalize ethers.js to avoid bundling it into the final build
-      external: ['ethers'], // This ensures ethers is treated as an external dependency
-      output: {
-        globals: {
-          ethers: 'ethers', // Makes ethers available globally when externalized
-        }
-      }
-    }
-  }
-});
+})
